0d5749dd6ad2990104c28105e2bf4ffa27d38867
fbs2015 <f.b.s2009@hotmail.com>
2021-07-12 17:06:58 -0300

    Created person class

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,10 +3,17 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome : String = "Fernando"
     var cpf : String = "999.999.999-77"
+    private set
+
+   //Um exemplo de inner class. Por consequencia, nesse caso, Pessoa() é uma outer class
+   // inner class Endereco{
+   //     var rua : String = "Ruma Minha Rua 666"
+   // }
 }
 
 fun main(){
     val p1 = Pessoa()
     println(p1.nome)
     println(p1.cpf)
+
 }
