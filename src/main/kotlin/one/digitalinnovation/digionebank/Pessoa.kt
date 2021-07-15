e6b3192118b58769007b4e4dadb812b38b614b97
fbs2015 <f.b.s2009@hotmail.com>
2021-07-15 10:43:04 -0300

    Criacao da classe Funcionario e teste rapido da classe

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,21 +1,7 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome : String = "Fernando"
-    var cpf : String = "999.999.999-77"
-    private set
+open class Pessoa(
+    open val nome : String,
+    open val cpf : String
+){}
 
-   //Um exemplo de inner class. Por consequencia, nesse caso, Pessoa() é uma outer class
-   // inner class Endereco{
-   //     var rua : String = "Ruma Minha Rua 666"
-   // }
-
-    constructor()//construtor secundario
-
-    fun infoPessoa() = "$nome e $cpf"
-}
-
-fun main(){
-    val p1 = Pessoa()
-    println(p1.infoPessoa())
-}
---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TestePessoa.kt
@@ -0,0 +1,8 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Pessoa
+
+fun main(){
+    val p1 = Pessoa("FBS", "999.999.999-77")
+    println(p1.nome)
+}
