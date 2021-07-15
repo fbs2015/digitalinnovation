59f4849854b047a255cd6ef8b51e3ed0ac0dcd06
fbs2015 <f.b.s2009@hotmail.com>
2021-07-15 13:59:02 -0300

    Classes Pessoa e Funcionario agora são abstract

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionario.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionario.kt
@@ -5,10 +5,10 @@ import one.digitalinnovation.digionebank.Pessoa
 import java.math.BigDecimal
 
 fun main(){
-    val p1 = Pessoa("FBS", "999.999.999-77")
+/*    val p1 = Pessoa("FBS", "999.999.999-77")
     println(p1.nome)
     println(p1.cpf)
-
+*/
     val f1 = Funcionario("FBS", "999.999.999-77", BigDecimal.valueOf(3000))
     println(f1.nome)
     println(f1.cpf)
--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TestePessoa.kt
+++
@@ -1,8 +0,0 @@
-package one.digitalinnovation.digionebank.testes
-
-import one.digitalinnovation.digionebank.Pessoa
-
-fun main(){
-    val p1 = Pessoa("FBS", "999.999.999-77")
-    println(p1.nome)
-}
